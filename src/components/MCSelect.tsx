59c36ce5c0aeff0219784c33e91d0c5618d5884c
olllllli <46918939+olllllli@users.noreply.github.com>
2022-01-26 23:49:25 +1100

    reorganised img folder slightly, setup dummy MCSelect

--- src/components/MCSelect.tsx
+++ src/components/MCSelect.tsx
@@ -1,1 +1,16 @@
-export { }
+import { FunctionComponent } from "react";
+
+interface MCSelectProps {
+
+}
+
+const MCSelect: FunctionComponent<MCSelectProps> = () => {
+    return (
+        <select>
+            <option value="9f2c6739-1f8f-4118-ab3a-2b102ee78311">_PC</option>
+            <option value="e059c39e-cd8c-4a09-bf3f-7142367f7cfb">5fps</option>
+        </select>
+    );
+};
+
+export default MCSelect;
