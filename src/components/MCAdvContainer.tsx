aa512187f848e8bb53bb48ea47674d8a5e41b629
olllllli <46918939+olllllli@users.noreply.github.com>
2022-01-27 03:22:19 +1100

    Finished most of the MCSelect functionality I believe, unstyled. Fixed the Container and View styling

--- src/components/MCAdvContainer.tsx
+++ src/components/MCAdvContainer.tsx
@@ -1,32 +1,37 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import MCAdvView from "./MCAdvView";
-import MCSelect from "./MCSelect";
+import { MCSelect, MCOption } from "./MCSelect";
 
 import "styles/components/MCAdvContainer.scss";
 
 /* The container gui for an advancement view, contains the selects and the view */
+// TODO: Possibly figure out how to only rerender the view
 const MCAdvContainer: FunctionComponent<{}> = () => {
+    const [category, setCategory] = useState<AdvCategory>("story");
+
+
+    console.log("rendered advContainer");
     return (
         <div className="MCAdvContainer">
             <div className="header">
                 <div>
                     <MCSelect>
-                        <option value="9f2c6739-1f8f-4118-ab3a-2b102ee78311">_PC</option>
-                        <option value="e059c39e-cd8c-4a09-bf3f-7142367f7cfb">5fps</option>
+                        <MCOption value="9f2c6739-1f8f-4118-ab3a-2b102ee78311" display="_PC" />
+                        <MCOption value="e059c39e-cd8c-4a09-bf3f-7142367f7cfb" display="5fps" />
                     </MCSelect>
                 </div>
                 <div>
-                    <MCSelect>
-                        <option value="story">Story</option>
-                        <option value="nether">Nether</option>
-                        <option value="end">End</option>
-                        <option value="adventure">Adventure</option>
-                        <option value="husbandry">Husbandry</option>
+                    <MCSelect onChange={setCategory}>
+                        <MCOption value="story" display="Story" selected={true} />
+                        <MCOption value="nether" display="Nether" />
+                        <MCOption value="end" display="End" />
+                        <MCOption value="adventure" display="Adventure" />
+                        <MCOption value="husbandry" display="Husbandry" />
                     </MCSelect>
                 </div>
             </div>
             <div className="view">
-                <MCAdvView category="story" />
+                <MCAdvView category={category} />
             </div>
         </div>
     );
--- src/components/MCSelect.tsx
+++ src/components/MCSelect.tsx
@@ -1,15 +1,62 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
+import * as React from 'react';
 
-interface MCSelectProps {
-
+/* A option component for the MCSelect component, doesn't render anything */
+// TODO: There might be a better interface for this
+interface MCOptionProps {
+    value: string;
+    display: string;
+    selected?: boolean;
 }
 
-const MCSelect: FunctionComponent<MCSelectProps> = (props) => {
+export const MCOption: FunctionComponent<MCOptionProps> = (props) => {
+    // For now returns an option
     return (
-        <select>
+        <option value={props.value}>{props.display}</option>
+    );
+};
+
+/* The Select component, should only be given MCOption as children */
+interface MCSelectProps {
+    onChange?: React.Dispatch<React.SetStateAction<any>>;
+}
+
+export const MCSelect: FunctionComponent<MCSelectProps> = (props) => {
+    // process the children, filter out anything that doesnt have the MCOptionProps
+    const children = React.Children.map(props.children, (child) => {
+        if (child && typeof child === "object" && "props" in child && ("value" in child.props && "display" in child.props)) {
+            return child.props as MCOptionProps;
+        }
+    });
+
+    // check the children, disable if no children
+    let isDisabled = false;
+    let blankOption: JSX.Element | undefined = <option disabled value="" style={{ display: "none" }} />;
+    let selectedValue = "";
+    const options = [];
+    if (!children || children.length === 0) {
+        // there were no children
+        isDisabled = true;
+    } else {
+        for (const child of children) {
+            // check if its selected (will only remember the last one)
+            if (child.selected) {
+                selectedValue = child.value;
+                blankOption = undefined;
+            }
+            // push it to the list of options
+            options.push(<option value={child.value}>{child.display}</option>);
+        }
+    }
+
+    // store the current value on the state
+    const [currentValue, setCurrentValue] = useState<string>(selectedValue);
+
+    // on change, setValue and call the given onChange function with the value
+    return (
+        <select disabled={isDisabled} value={currentValue} onChange={(e) => { if (props.onChange) { props.onChange(e.target.value); }; setCurrentValue(e.target.value); }}>
+            {blankOption}
             {props.children}
         </select>
     );
 };
-
-export default MCSelect;
