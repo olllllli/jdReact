0756f36a18c0b33d258100e3a22a1dea046fb119
olllllli <46918939+olllllli@users.noreply.github.com>
2022-01-29 22:47:20 +1100

    wip: Setup basic `MCStat`

---
+++ src/components/MCStat.tsx
@@ -0,0 +1,26 @@
+import { FunctionComponent } from "react";
+
+import "styles/components/MCStat.scss";
+
+
+/* A component to display a statistic, has to be given the data */
+interface MCStatProps {
+    player: playerData;
+    displayedValue: number;
+}
+
+// TODO: make the names link to their player page once that is finished
+// TODO: Use a custom component for the icon instead of img
+const MCStat: FunctionComponent<MCStatProps> = (props) => {
+    return (
+        <div className="MCStat">
+            <div className="player">
+                <img className="icon" />
+                <span className="username">{props.player.username}</span>
+            </div>
+            <span className="value">{props.displayedValue}</span>
+        </div>
+    );
+};
+
+export default MCStat;
--- src/routes/StatsPage.tsx
+++ src/routes/StatsPage.tsx
@@ -5,6 +5,10 @@ import { Link } from "react-router-dom";
 import "styles/routes/common.scss";
 import "styles/gui.scss";
 import written_book from "img/resourcepacks/vanilla/assets/minecraft/textures/item/written_book.png";
+import MCStat from "components/MCStat";
+
+// DEBUG
+const me = { "username": "_PC", "skin": "http://textures.minecraft.net/texture/cc69b53431dfaa7f60e65dd844f76273731d4074f2dc53e08cd42e7c74fa7392", "offline": false, "advancementCount": 78 };
 
 const StatsPage: FunctionComponent<{}> = () => {
     return (
@@ -16,7 +20,11 @@ const StatsPage: FunctionComponent<{}> = () => {
                     <h2>Statistics</h2>
                 </MCToast>
             </header>
-            <main></main>
+            <main>
+                <div style={{ width: 450 }}>
+                    <MCStat player={me} displayedValue={100} />
+                </div>
+            </main>
         </>
     );
 };
